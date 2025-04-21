f31a7eaa821acb459640810c37ab6894c96d1f41
Astgenne <danicandido02@gmail.com>
2025-04-22 01:11:10 +0200

    created workflow for deployment; started home page creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GameOfLifeBgComponent, PATTERN_NAMES } from 'ngx-game-of-life-bg';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, NavbarComponent, GameOfLifeBgComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'atgenne4922.github.io';
+    pattern = PATTERN_NAMES[Math.floor(Math.random() * PATTERN_NAMES.length)];
 }
